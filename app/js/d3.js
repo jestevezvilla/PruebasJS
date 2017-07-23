10ba4f3f29413cdfebf7de5c47535333ab22fea9
Javier Estévez Villa <jestevez.villa@gmail.com>
2017-07-23 18:40:43 +0200

    D3JS

--- app/js/d3.js
+++ app/js/d3.js
@@ -4,23 +4,83 @@ import '../css/d3.scss';
 
 d3.select('body').append('p').text('New paragraph!');
 
-d3.select('body').append('div').attr('id', 'graph');
-
-const dataset = [12, 32, 43, 32, 13, 43];
+d3.select('body').append('div').attr('id', 'graph1');
+d3.select('body').append('div').attr('id', 'graph2');
 
 const w = 500;
 const h = 200;
 
-const svg = d3.select('#graph')
+const svg1 = d3.select('#graph1')
                 .append('svg')
                   .attr('height', h)
                   .attr('width', w);
 
-svg
+const dataset1 = [12, 32, 43, 32, 13, 43];
+
+svg1
+  .selectAll('rect')
+  .data(dataset1)
+  .enter()
+  .append('rect')
+    .attr('x', (d, i) => (i * w) / dataset1.length)
+    .attr('y', d => h - d)
+    .attr('width', -2 + (w / dataset1.length))
+    .attr('height', d => d)
+    .attr('fill', d => `rgb(0,0, ${d * 10})`);
+
+svg1
+  .selectAll('text')
+  .data(dataset1)
+  .enter()
+  .append('text')
+    .text(d => d)
+    .attr('x', (d, i) => (i * (w / dataset1.length)) + (((w / dataset1.length) - 2) / 2))
+    .attr('y', d => (h - d) + 15)
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white');
+
+
+// const w = 500;
+// const h = 200;
+
+
+const dataset2 = [[12, 32], [43, 32], [13, 43], [21, 32], [54, 14], [54, 1]];
+
+const pad = 20;
+
+const xScale =
+  d3.scaleLinear()
+    .domain([0, d3.max(dataset2, d => d[0])])
+    .range([pad, w - pad]);
+
+const yScale =
+  d3.scaleLinear()
+    .domain([0, d3.max(dataset2, d => d[1])])
+    .range([h - pad, pad]);
+
+const rScale =
+  d3.scaleLinear()
+    .domain([0, d3.max(dataset2, d => d[1])])
+    .range([1, 10]);
+
+const xAxis = d3.axisBottom(xScale);
+
+const svg2 = d3.select('#graph2')
+                .append('svg')
+                  .attr('height', h)
+                  .attr('width', w);
+
+
+svg2
+  .append('g')
+  .call(xAxis);
+
+svg2
   .selectAll('circle')
-  .data(dataset)
+  .data(dataset2)
   .enter()
   .append('circle')
-    .attr('cy', h / 2)
-    .attr('cx', (d, i) => (i * 25) + (d * 2))
-    .attr('r', d => d);
+    .attr('cx', d => xScale(d[0]))
+    .attr('cy', d => yScale(d[1]))
+    .attr('r', d => rScale(d[1]));
+
